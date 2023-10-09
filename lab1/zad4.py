337c25cf37349bdb634926663b3457ddd86048e4
Adam Szreiber <adam.szr98@gmail.com>
2023-10-09 20:19:32 +0200

    lab 1, zad 4

--- lab1/zad4.py
+++ lab1/zad4.py
@@ -14,10 +14,16 @@ acctual_temp = req.get('current').get('temperature_2m')
 temperature_tomorrow_max = req.get('daily').get('temperature_2m_max')[1]
 temperature_tomorrow_min = req.get('daily').get('temperature_2m_min')[1]
 
+# Pobieramy aktualną datę i czas
+aktualna_data_i_czas = datetime.now()
 
-print('\r\n------------ Bydgoszcz ------------')
-print('Aktualna temperatura: '+str(acctual_temp)+'°C')
+# Formatujemy datę zgodnie z Twoimi wymaganiami
+sformatowana_data = aktualna_data_i_czas.strftime("%d.%m.%Y %H:%M")
+
+print('\r\n\r\n')
+print('Data: '+sformatowana_data)
 print('')
-print('Jutro:')
-print('Minimalna temp: '+str(temperature_tomorrow_min)+'°C')
-print('Maksymalna temp: '+str(temperature_tomorrow_max)+'°C')
+print('Temperatura: '+str(acctual_temp)+'°C')
+print('')
+print('Jutro: '+str(temperature_tomorrow_max)+'/'+str(temperature_tomorrow_min)+'°C')
+print('\r\n\r\n')
